f3c42b1620ab4dd9042b86d50e7a6d35f09191b2
sampad789 <sampad789@gmail.com>
2020-04-16 12:27:01 +0300

    Added Navbar

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+
+import SEO from "../components/seo"
+
+const Aboutpage = () => (
+  <Layout>
+    <SEO title="About" />
+    <h1>You've hit about page</h1>
+  </Layout>
+)
+
+export default Aboutpage
